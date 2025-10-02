d908f40b5020ad71774838bef1ce50ea21231798
GustavoH06 <gustavocaixeta7@hotmail.com>
2025-10-02 19:36:32 +0000

    adicionado todos elementos da home

---
+++ src/components/hero/Speech.jsx
@@ -0,0 +1,10 @@
+const Speech = () => {
+  return (
+    <div className='bubbleContainer'>
+        <div className="bubble">Test</div>
+        <img src="/man.png" alt="" />
+    </div>
+  )
+}
+
+export default Speech
