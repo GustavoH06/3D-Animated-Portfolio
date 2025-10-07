6f32b01a36f78ad945f7eb71cd5ee0c475d1fce6
GustavoH06 <gustavocaixeta7@hotmail.com>
2025-10-07 17:06:51 +0000

    termino de estilização e introdução de animações

--- src/components/hero/Speech.jsx
+++ src/components/hero/Speech.jsx
@@ -1,7 +1,24 @@
+import { TypeAnimation } from "react-type-animation";
+import "./hero.css";
+
 const Speech = () => {
   return (
     <div className='bubbleContainer'>
-        <div className="bubble">Test</div>
+        <div className="bubble">
+          <TypeAnimation
+            sequence={[
+              1000,
+              "Seja bem vindo a meu portfolio",
+              1000,
+              "Lorem ipsum dolor sit amet lerinat consectetur adipisicing. ",
+              1000,
+            ]}
+            wrapper="span"
+            speed={40}
+            deletionSpeed={50}
+            repeat={Infinity}
+          />
+        </div>
         <img src="/man.png" alt="" />
     </div>
   )
